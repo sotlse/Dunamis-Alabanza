c0fed525e7ac0436938112e59a41445bbc5688c9
Glitch (hello-webpage) <none>
2019-05-10 22:31:17 +0000

    🐏🍧 Checkpoint ./index.html:4132453/31 ./script.js:4132453/203

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+const canciones = [
+  {
+    titulo: "A Cada Instante",
+    autor: "B",
+    tono: "B"
+  },
+  
+  
+  
+  ]
+  
+
+
