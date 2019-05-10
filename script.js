ab3343adb0e2b8a9c731bfde7e6b4ccd0e524882
Glitch (hello-webpage) <none>
2019-05-10 22:41:19 +0000

    🗺👷 Checkpoint ./script.js:4132453/627

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
 const canciones = [
   {
+    pagina:"A_Cada_Instante.html",
     titulo: "A Cada Instante",
     autor: "B",
     tono: "B"
   },
   
+  {
+    pagina: "A_El_Alto_Y_Sublime.html",
+    titulo: "A el Alto y Sublime",
+    autor: "Jesus Adrian Romero",
+    tono: "Bb"
+  },
+  
+  {
+    pagina:"Abre_los_Cielos.html",
+    titulo: "Abre los Cielos",
+    autor: "ds",
+    tono: "D"
+  }
   
   
-  ]
+]
   
 
 
