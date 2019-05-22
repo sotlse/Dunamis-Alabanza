daaf743915c8fdf4442bf7698270920098d2d8a0
Glitch (hello-webpage) <none>
2019-05-22 02:42:13 +0000

    ☕️👣 Checkpoint ./landscape.js:4132453/676

--- landscape.js
+++ landscape.js
@@ -1,1 +1,15 @@
-window.screen.lockOrientation('portrait');
+fullScreen();
+screen.orientation.lock('landscape-primary');
+
+function fullScreen() {
+  // Kind of painful, but this is how it works for now
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+}
