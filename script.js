bc8ff305349268cd4431d1c890ab10dd6f8dbd33
Glitch (hello-webpage) <none>
2019-05-24 23:03:57 +0000

    🌒🚦 Checkpoint ./script.js:4132453/540

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ busquedaItem.addEventListener("keyup",filterItems);
 
 //botonAgregar function
 function agregarCanto(e){
-  if(e.target.classList.contains("boton-add"))
-  console.log(1);
-  
+  if(e.target.classList.contains("boton-add")){
+  var myWindow = window.open("", "", "width=200, height=100");   // Opens a new window
+myWindow.document.write("<p>A new window!</p>");      // Some text in the new window
+myWindow.blur();                          // Assures that the new window does NOT get focus
+  }
 }
 
 
