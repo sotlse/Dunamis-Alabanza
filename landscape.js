614360266ba8fbd961b2caa6592c424bdcc31987
Glitch (dunamis-alabanza) <none>
2019-12-21 18:03:43 +0000

    🍜👓 Checkpoint ./landscape.js:4132453/158

--- landscape.js
+++ landscape.js
@@ -1,10 +1,10 @@
-/*fullScreen();
-ScreenOrientation.lock('landscape-primary');
+fullScreen();
+screen.orientation.lock("landscape-primary");
 
 function fullScreen() {
   // Kind of painful, but this is how it works for now
   if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
+    document.querySelector(".canto_body").requestFullscreen();
   } else if (document.documentElement.mozRequestFullScreen) {
     document.documentElement.mozRequestFullScreen();
   } else if (document.documentElement.webkitRequestFullscreen) {
@@ -12,7 +12,7 @@ function fullScreen() {
   } else if (document.documentElement.msRequestFullscreen) {
     document.documentElement.msRequestFullscreen();
   }
-}*/
+}
 
 var current_mode = screen.orientation;
 
@@ -22,13 +22,13 @@ console.log(current_mode.type)
 // angle
 console.log(current_mode.angle)
 
-if(document.documentElement.requestFullscreen)
+if (document.documentElement.requestFullscreen)
 		console.log("Hola");
-
+/*
 screen.orientation.lock("landscape-primary")
 .then(function() {
   alert('Locked');
 })
 .catch(function(error) {
   alert(error);
-});
+});*/
