fdb070e4a38a386413aeea93eb4a6c04d7309490
Glitch (dunamis-alabanza) <none>
2019-12-21 18:15:41 +0000

    🌟👜 Checkpoint ./landscape.js:4132453/620 ./Cantos/Acordes/Oh_Ven_Bendito_Emanuel_Acordes.html:4132453/384

--- landscape.js
+++ landscape.js
@@ -1,10 +1,18 @@
+var current_mode = screen.orientation;
+
+// type
+console.log(current_mode.type)
+
+// angle
+console.log(current_mode.angle)
+
 fullScreen();
 screen.orientation.lock("landscape-primary");
 
 function fullScreen() {
   // Kind of painful, but this is how it works for now
   if (document.documentElement.requestFullscreen) {
-    document.querySelector(".canto_body").requestFullscreen();
+    document.documentElement.requestFullscreen();
   } else if (document.documentElement.mozRequestFullScreen) {
     document.documentElement.mozRequestFullScreen();
   } else if (document.documentElement.webkitRequestFullscreen) {
@@ -14,21 +22,3 @@ function fullScreen() {
   }
 }
 
-var current_mode = screen.orientation;
-
-// type
-console.log(current_mode.type)
-
-// angle
-console.log(current_mode.angle)
-
-if (document.documentElement.requestFullscreen)
-		console.log("Hola");
-/*
-screen.orientation.lock("landscape-primary")
-.then(function() {
-  alert('Locked');
-})
-.catch(function(error) {
-  alert(error);
-});*/
