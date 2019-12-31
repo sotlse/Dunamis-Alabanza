de33dfadef918aa8ac799e3b6c1b647c619cfc6a
Glitch (dunamis-alabanza) <none>
2019-12-31 16:19:08 +0000

    🏆🛩 Checkpoint ./jquery.transposer.js:4132453/4190

--- jquery.transposer.js
+++ jquery.transposer.js
@@ -11,154 +11,154 @@
 (function($) {
 
     $.fn.transpose = function(options) {
-      var opts = $.extend({}, $.fn.transpose.defaults, options);
+        var opts = $.extend({}, $.fn.transpose.defaults, options);
       
-      var currentKey = null;
+        var currentKey = null;
       
-      var keys = [
-        { name: 'Ab',  value: 0,   type: 'F' },
-        { name: 'A',   value: 1,   type: 'N' },
-        { name: 'A#',  value: 2,   type: 'S' },
-        { name: 'Bb',  value: 2,   type: 'F' },
-        { name: 'B',   value: 3,   type: 'N' },
-        { name: 'C',   value: 4,   type: 'N' },
-        { name: 'C#',  value: 5,   type: 'S' },
-        { name: 'Db',  value: 5,   type: 'F' },
-        { name: 'D',   value: 6,   type: 'N' },
-        { name: 'D#',  value: 7,   type: 'S' },
-        { name: 'Eb',  value: 7,   type: 'F' },
-        { name: 'E',   value: 8,   type: 'N' },
-        { name: 'F',   value: 9,   type: 'N' },
-        { name: 'F#',  value: 10,  type: 'S' },
-        { name: 'Gb',  value: 10,  type: 'F' },
-        { name: 'G',   value: 11,  type: 'N' },
-        { name: 'G#',  value: 0,   type: 'S' }
-      ];
+        var keys = [
+            { name: 'Ab',  value: 0,   type: 'F' },
+            { name: 'A',   value: 1,   type: 'N' },
+            { name: 'A#',  value: 2,   type: 'S' },
+            { name: 'Bb',  value: 2,   type: 'F' },
+            { name: 'B',   value: 3,   type: 'N' },
+            { name: 'C',   value: 4,   type: 'N' },
+            { name: 'C#',  value: 5,   type: 'S' },
+            { name: 'Db',  value: 5,   type: 'F' },
+            { name: 'D',   value: 6,   type: 'N' },
+            { name: 'D#',  value: 7,   type: 'S' },
+            { name: 'Eb',  value: 7,   type: 'F' },
+            { name: 'E',   value: 8,   type: 'N' },
+            { name: 'F',   value: 9,   type: 'N' },
+            { name: 'F#',  value: 10,  type: 'S' },
+            { name: 'Gb',  value: 10,  type: 'F' },
+            { name: 'G',   value: 11,  type: 'N' },
+            { name: 'G#',  value: 0,   type: 'S' }
+        ];
     
-      var getKeyByName = function (name) {
-          if (name.charAt(name.length-1) == "m") {
-            name = name.substring(0, name.length-1);
-          }
-          for (var i = 0; i < keys.length; i++) {
-              if (name == keys[i].name) {
-                  return keys[i];
-              }
-          }
-      };
-  
-      var getChordRoot = function (input) {
-          if (input.length > 1 && (input.charAt(1) == "b" || input.charAt(1) == "#"))
-              return input.substr(0, 2);
-          else
-              return input.substr(0, 1);
-      };
-  
-      var getNewKey = function (oldKey, delta, targetKey) {
-          var keyValue = getKeyByName(oldKey).value + delta;
-  
-          if (keyValue > 11) {
-              keyValue -= 12;
-          } else if (keyValue < 0) {
-              keyValue += 12;
-          }
-          
-          var i=0;
-          if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) {
-              // Return the Flat or Sharp Key
-              switch(targetKey.name) {
-                case "A":
-                case "A#":
-                case "B":
-                case "C":
-                case "C#":
-                case "D":
-                case "D#":
-                case "E":
-                case "F#":
-                case "G":
-                case "G#":
-                    for (;i<keys.length;i++) {
-                      if (keys[i].value == keyValue && keys[i].type == "S") {
-                        return keys[i];
-                      }
-                    }
-                default:
-                    for (;i<keys.length;i++) {
-                      if (keys[i].value == keyValue && keys[i].type == "F") {
-                        return keys[i];
-                      }
-                    }
-              }
-          }
-          else {
-              // Return the Natural Key
-              for (;i<keys.length;i++) {
-                if (keys[i].value == keyValue) {
-                  return keys[i];
+        var getKeyByName = function (name) {
+            if (name.charAt(name.length-1) == "m") {
+                name = name.substring(0, name.length-1);
+            }
+            for (var i = 0; i < keys.length; i++) {
+                if (name == keys[i].name) {
+                    return keys[i];
                 }
-              }
-          }
-      };
+            }
+        };
   
-      var getChordType = function (key) {
-          switch (key.charAt(key.length - 1)) {
-              case "b":
-                  return "F";
-              case "#":
-                  return "S";
-              default:
-                return "N";
-          }
-      };
+        var getChordRoot = function (input) {
+            if (input.length > 1 && (input.charAt(1) == "b" || input.charAt(1) == "#"))
+                return input.substr(0, 2);
+            else
+                return input.substr(0, 1);
+        };
   
-      var getDelta = function (oldIndex, newIndex) {
-          if (oldIndex > newIndex)
-              return 0 - (oldIndex - newIndex);
-          else if (oldIndex < newIndex)
-              return 0 + (newIndex - oldIndex);
-          else
-              return 0;
-      };
-  
-      var transposeSong = function (target, key) {
-          var newKey = getKeyByName(key);
-          console.log(newKey.name); 
-          if (currentKey.name == newKey.name) {
-            return;
-          }
-  
-          var delta = getDelta(currentKey.value, newKey.value);
+        var getNewKey = function (oldKey, delta, targetKey) {
+            var keyValue = getKeyByName(oldKey).value + delta;
+    
+            if (keyValue > 11) {
+                keyValue -= 12;
+            } else if (keyValue < 0) {
+                keyValue += 12;
+            }
           
-          $("span.c", target).each(function (i, el) {
-              transposeChord(el, delta, newKey);
-          });
-          
-          currentKey = newKey;
-          localStorage.setItem("TonoActual",JSON.stringify(currentKey.name));
+            var i=0;
+            if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) {
+                // Return the Flat or Sharp Key
+                switch(targetKey.name) {
+                    case "A":
+                    case "A#":
+                    case "B":
+                    case "C":
+                    case "C#":
+                    case "D":
+                    case "D#":
+                    case "E":
+                    case "F#":
+                    case "G":
+                    case "G#":
+                        for (;i<keys.length;i++) {
+                        if (keys[i].value == keyValue && keys[i].type == "S") {
+                            return keys[i];
+                        }
+                        }
+                    default:
+                        for (;i<keys.length;i++) {
+                        if (keys[i].value == keyValue && keys[i].type == "F") {
+                            return keys[i];
+                        }
+                        }
+                }
+            }
+            else {
+                // Return the Natural Key
+                for (;i<keys.length;i++) {
+                    if (keys[i].value == keyValue) {
+                    return keys[i];
+                    }
+                }
+            }
+        };
+  
+        var getChordType = function (key) {
+            switch (key.charAt(key.length - 1)) {
+                case "b":
+                    return "F";
+                case "#":
+                    return "S";
+                default:
+                    return "N";
+            }
+        };
+  
+        var getDelta = function (oldIndex, newIndex) {
+            if (oldIndex > newIndex)
+                return 0 - (oldIndex - newIndex);
+            else if (oldIndex < newIndex)
+                return 0 + (newIndex - oldIndex);
+            else
+                return 0;
+        };
+  
+        var transposeSong = function (target, key) {
+            var newKey = getKeyByName(key);
+            console.log(newKey.name); 
+            if (currentKey.name == newKey.name) {
+                return;
+            }
+    
+            var delta = getDelta(currentKey.value, newKey.value);
+            
+            $("span.c", target).each(function (i, el) {
+                transposeChord(el, delta, newKey);
+            });
+            
+            currentKey = newKey;
+            localStorage.setItem("TonoActual",JSON.stringify(currentKey.name));
 
-          /*------------Cambiar Tono de class = Tono en la cabecera del canto------------*/
-          var tono = $(".AcordeCanto").text()
-          //Si tiene un bemol o sostenido, entonces removerlo
-          if (tono.length>1)
-            tono = tono.substr(0,1);
-          var raiz = tono.replace(/[A-Z]/g,currentKey.name);
-          $(".AcordeCanto").html(raiz);
-      };
+            /*------------Cambiar Tono de class = Tono en la cabecera del canto------------*/
+            var tono = $(".AcordeCanto").text()
+            //Si tiene un bemol o sostenido, entonces removerlo
+            if (tono.length>1)
+                tono = tono.substr(0,1);
+            var raiz = tono.replace(/[A-Z]/g,currentKey.name);
+            $(".AcordeCanto").html(raiz);
+        };
   
-      var transposeChord = function (selector, delta, targetKey) {
-          var el = $(selector);
-          var oldChord = el.text();
-          var oldChordRoot = getChordRoot(oldChord);
-          var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
-          var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length);
-          el.text(newChord);
-  
-          var sib = el[0].nextSibling;
-          if (sib && sib.nodeType == 3 && sib.nodeValue.length > 0 && sib.nodeValue.charAt(0) != "/") {
-              var wsLength = getNewWhiteSpaceLength(oldChord.length, newChord.length, sib.nodeValue.length);
-              sib.nodeValue = makeString(" ", wsLength);
-          }
-      };
+        var transposeChord = function (selector, delta, targetKey) {
+            var el = $(selector);
+            var oldChord = el.text();
+            var oldChordRoot = getChordRoot(oldChord);
+            var newChordRoot = getNewKey(oldChordRoot, delta, targetKey);
+            var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length);
+            el.text(newChord);
+    
+            var sib = el[0].nextSibling;
+            if (sib && sib.nodeType == 3 && sib.nodeValue.length > 0 && sib.nodeValue.charAt(0) != "/") {
+                var wsLength = getNewWhiteSpaceLength(oldChord.length, newChord.length, sib.nodeValue.length);
+                sib.nodeValue = makeString(" ", wsLength);
+            }
+        };
   
       var getNewWhiteSpaceLength = function (a, b, c) {
           if (a > b)
@@ -175,22 +175,7 @@
           return o.join("");
       }
       
-      //Ver en cada linea si es una linea de acordes o de letra
-      var isChordLine = function (input) {
-          //var tokens = input.replace(/\s+/, " ").split(" ");
-          var tokens = input.replace(/\s+/, " ").replace(/-|[|]/g, " ").split(" ");
-          // Try to find tokens that aren't chords
-          // if we find one we know that this line is not a 'chord' line.
-          for (var i = 0; i < tokens.length; i++) {
-              if (!$.trim(tokens[i]).length == 0 && !tokens[i].match(opts.chordRegex))
-                  return false;
-          }
-          return true;
-      };
       
-      var wrapChords = function (input) {
-          return input.replace(opts.chordReplaceRegex, "<span class='c'>$1</span>");
-      };
       
       
       return $(this).each(function() {
@@ -211,7 +196,7 @@
         currentKey = getKeyByName(startKey);
         startKey = startKey.substr(0,1);
   
-        // Build tranpose links ===========================================
+        /*----------------------Construir la botonera para transportar----------------------*/
         var keyLinks = [];
         $(keys).each(function(i, key) {
             if (key.name == startKey)
@@ -252,7 +237,7 @@
         console.log(startKey);
         console.log(TonoActual);
         
-        /*----------------------Agarrar todas las lineas del <pre>----------------------*/
+        /*----------------------Agarrar todas las lineas del <pre> y revisar si es acorde o un titulo----------------------*/
         var output = [];
         var lines = $(this).text().split(/\r\n|\n/g);
         var line, tmp = "";
@@ -279,8 +264,8 @@
   
   
     $.fn.transpose.defaults = {
-      chordRegex: /^[A-G][b\#]?(2|4|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|b5|#5|#9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4)*(\/[A-G][b\#]*)*$/,
-  chordReplaceRegex: /([A-G][b\#]?(2|4|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|b5|#5|#9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4)*)/g
+        chordRegex: /^[A-G][b\#]?(2|4|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|b5|#5|#9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4)*(\/[A-G][b\#]*)*$/,
+        chordReplaceRegex: /([A-G][b\#]?(2|4|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|b5|#5|#9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4)*)/g
     };
   
-  })(jQuery);
+})(jQuery);
