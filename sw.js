f40593758b593b4c66a5c66826df2690cda933c5
Glitch (dunamis-alabanza) <none>
2020-01-03 23:06:59 +0000

    🚛🛥 Checkpoint ./sw.js:4132453/504

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 const cacheName = 'cache-v1';
-const urlsToCache = [
+/*const urlsToCache = [
   '/',
   '/style.css',
   '/script.js'
-];
+];*/
 
 /*-------------------------LLAMAR ELEMENTO INSTALAR-------------------------*/
 self.addEventListener('install',e => {
   console.log('Service Worker: Installed');
   // Perform install steps
-  e.waitUntil(
+  /*e.waitUntil(
     caches.open(cacheName)
       .then(cache => {
         console.log('Opened cache');
         cache.addAll(urlsToCache);
       })
       .then(() => self.skipWaiting())
-  );
+  );*/
 });
 
 /*-------------------------LLAMAR ELEMENTO ACTIVAR-------------------------*/
@@ -41,16 +41,27 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.reques))
-      .then(function(response) {
+    fetch(e.request)
+    .then(res => {
+      //Make copy/clone of response
+      const respClone = res.clone();
+      //Open cache
+      caches.open(cacheName)
+        .then(cache => {
+          //Add response to cache
+          cache.put(e.request, respClone);
+        });
+      return res;
+    }).catch(err => caches)
+  );
+  //e.respondWith(fetch(e.request).catch(() => caches.match(e.request))
+      /*.then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
-  );
+      })*/
 });
 
 
