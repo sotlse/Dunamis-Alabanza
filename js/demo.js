98504e899b4209308212e8c533f6a159e4eec42f
Glitch (dunamis-alabanza) <none>
2020-01-06 04:54:40 +0000

    🧀👏 Checkpoint ./Calendario.html:4132453/10 ./js/demo.js:4132453/10

--- js/demo.js
+++ js/demo.js
@@ -22,16 +22,16 @@ $(document).ready( function() {
     var thisMonth = moment().format('YYYY-MM');
     // Events to load into calendar
     var eventArray = [
-        {
+        /*{
             title: 'Multi-Day Event',
             endDate: thisMonth + '-14',
             startDate: thisMonth + '-10'
-        }, {
+        },*/ {
             endDate: thisMonth + '-23',
             startDate: thisMonth + '-21',
             title: 'Another Multi-Day Event'
         }, {
-            date: thisMonth + '-27',
+            date: thisMonth + '-26',
             title: 'Single Day Event'
         }
     ];
