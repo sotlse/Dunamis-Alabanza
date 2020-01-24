ef659abc2874e8c5b80a223f3feb463ee99e1cd7
Glitch (dunamis-alabanza) <none>
2020-01-24 01:15:18 +0000

    🎅🏤 Checkpoint ./js/pagina-cancion-acordes.js:4132453/3327 ./Cantos/Acordes/Tu_Eres_Dios_EEYV_Acordes.html:4132453/639

--- js/pagina-cancion-acordes.js
+++ js/pagina-cancion-acordes.js
@@ -10,6 +10,21 @@ const h3 = document.querySelector('.autorDelCanto').textContent;
 const pre = document.querySelector('.canto_body').innerHTML;
 const tono = document.querySelector('.tono').innerHTML;
 const tempo = document.querySelector('.tempo').innerHTML;
+const head = document.queryqquerySelector("head");
+
+console.log(head);
+
+let HeadTemaplate  =
+    "<meta charset='utf-8'>" + 
+    "<meta http-equiv='X-UA-Compatible' content='IE=edge'>" +
+    "<meta name='viewport' content='width=device-width, initial-scale=1'>" +
+
+    "<!-- import the webpage's stylesheet -->" +
+    "<link rel='stylesheet' href='/style.css'>" + 
+    "<link rel='manifest' href='/manifest.json'>" +
+    "<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>" +
+    "<link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>";
+
 
 let BodyTemplate =
     "<div class='encabezado-lista'>" +
@@ -82,6 +97,6 @@ let BodyTemplate =
     '</nav>';
 
     console.log(BodyTemplate);
-let HeadTemaplate  =
-    
+
+
 //}
