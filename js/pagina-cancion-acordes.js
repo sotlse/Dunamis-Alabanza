c41de844db53576d1648bb1a1e09e562a69796f8
Glitch (dunamis-alabanza) <none>
2020-01-24 05:24:45 +0000

    🎬😬 Checkpoint ./js/pagina-cancion-acordes.js:4132453/495 ./Cantos/Acordes/Tu_Eres_Dios_EEYV_Acordes.html:4132453/211

--- js/pagina-cancion-acordes.js
+++ js/pagina-cancion-acordes.js
@@ -15,7 +15,8 @@ const head = document.querySelector("head");
 console.log(head);
 
 let HeadTemaplate  =
-    "<meta charset='utf-8'>" + 
+    "<title>" + `${title}` + "</title>" +
+    "<meta charset='utf-8'>" +
     "<meta http-equiv='X-UA-Compatible' content='IE=edge'>" +
     "<meta name='viewport' content='width=device-width, initial-scale=1'>" +
 
