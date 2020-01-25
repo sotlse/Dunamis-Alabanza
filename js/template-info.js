ff9499f3f1695b07f6732a19638ee386a84531fd
Glitch (dunamis-alabanza) <none>
2020-01-25 06:00:04 +0000

    🐘🕰 Checkpoint ./Cantos/Info/Tu_Eres_Dios_EEYV.html:4132453/151 ./js/template-info.js:4132453/1055 ./Cantos/Acordes/Tu_Eres_Dios_EEYV_Acordes.html:4132453/154

--- js/template-info.js
+++ js/template-info.js
@@ -9,3 +9,35 @@ let HeadTemplate =
     '<link rel="stylesheet" href="/style.css">' +
     '<link rel="manifest" href="/manifest.json">' +
     '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">';
+
+let BodyTemplate = 
+    '<header class="header">' +
+      '<div class="encabezado-lista">' +
+        `<h2>Tu eres Dios</h2>` +
+        `<h3>En Espiritu y Verdad</h3>` +
+        '<div class ="arrowRegresar">' +
+          '<i class="fas fa-arrow-left"></i>' +
+        '</div>' +
+      '</div>' +
+    '</header>' +
+    
+    '<hr>' +
+        
+    <div class="cuerpo-lista">
+      <ul id="opcionesDeCanto">
+        <li class="lista"> <a href="/Cantos/Letra/Tu_Eres_Dios_EEYV_Letra.html">Letra</a> <button class="boton-add">
+          +</button></li>
+          
+        <li class="lista"> <a href="/Cantos/Acordes/Tu_Eres_Dios_EEYV_Acordes.html">Acordes</a> <button class="boton-add">
+          +</button></li>
+      </ul>
+    </div>
+      
+    '<div id="dialogverlay"></div>' +
+    '<div id="dialogbox">' +
+      '<div>' +
+        '<div id="dialogboxhead"></div>' +
+        '<div id="dialogboxbody"></div>' +
+        '<div id="dialogboxfoot"></div>' +
+      '</div>' +
+    '</div>';
